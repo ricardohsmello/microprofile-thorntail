04a648630e2b6e4cf11034142129e567549cfe16
ricardo.mello <ricardo.mello@senior.com.br>
2019-07-25 22:07:25 -0300

    commit

---
+++ ricas-microprofile/src/main/java/com/ricas/microprofile/ricas/microprofile/GreeterRestController.java
@@ -0,0 +1,68 @@
+package com.ricas.microprofile.ricas.microprofile;
+
+import io.opentracing.*;
+import io.opentracing.Scope;
+import io.opentracing.contrib.jaxrs2.client.*;
+import io.opentracing.contrib.tracerresolver.*;
+import io.opentracing.propagation.*;
+import org.eclipse.microprofile.config.inject.*;
+import org.eclipse.microprofile.faulttolerance.*;
+import org.eclipse.microprofile.opentracing.*;
+
+import javax.inject.*;
+import javax.ws.rs.*;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.*;
+import java.net.*;
+import java.util.*;
+
+@Path("/api")
+public class GreeterRestController {
+
+    @Inject
+    @ConfigProperty(name="greeting.saying", defaultValue = "Hello")
+    private String saying;
+
+    @Inject
+    @ConfigProperty(name = "greeting.backendServiceHost",  defaultValue = "localhost")
+    private String backendServiceHost;
+
+    @Inject
+    @ConfigProperty(name = "greeting.backendServicePort", defaultValue = "8080")
+    private int backendServicePort;
+
+
+    @GET
+    @Produces("text/plain")
+    @Path("greeting")
+    @CircuitBreaker
+    @Timeout
+    @Fallback(fallbackMethod = "fallback")
+    @Traced
+    public String greeting() {
+
+        String backendServiceUrl = String.format("http://%s:%d",
+                backendServiceHost,backendServicePort);
+
+        System.out.println("Sending to: " + backendServiceUrl);
+
+        Client client = ClientTracingRegistrar
+                .configure(ClientBuilder.newBuilder()).build();
+
+        BackendDTO backendDTO = client.target(backendServiceUrl)
+                .path("api")
+                .path("backend")
+                .queryParam("greeting", saying)
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get(BackendDTO.class);
+
+        return backendDTO.getGreeting()
+                + " at host: " + backendDTO.getIp();
+    }
+
+    public String fallback(){
+        return saying + " at host "  +
+        System.getenv("HOSTNAME") + " - (fallback)";
+    }
+
+}
---
+++ ricas-microprofile/src/main/java/com/ricas/microprofile/ricas/microprofile/HelloRestController.java
@@ -0,0 +1,35 @@
+package com.ricas.microprofile.ricas.microprofile;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+
+@Path("/api")
+public class HelloRestController {
+
+    @Inject
+    @ConfigProperty(name="helloapp.saying")
+    private String saying;
+
+    @GET
+    @Produces("text/plain")
+    @Path("/hello")
+    public String hello() {
+        String hostname = null;
+        try {
+            hostname = InetAddress.getLocalHost()
+                    .getHostAddress();
+        } catch (UnknownHostException e) {
+            hostname = "unknown";
+        }
+        return saying + " " + hostname;
+    }
+
+}
